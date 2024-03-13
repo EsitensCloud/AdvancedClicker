397a4433fd810b98a44f40609bbb8f4a8585cc06
Danya <danya.bublichenko@mail.ru>
2024-03-13 09:10:15 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+
+  const [counter, setCount] = useState(0)
+
+  const upClick = () => {
+    setCount(counter + 1)
+  }
+
+  const downClick = () => {
+    setCount(counter - 1)
+  }
+
+  const resetClick = () => {
+    setCount(0)
+  }
+
+  let background;
+  if (counter < 0) {
+    background = "red-background"
+  }
+
+  else if (counter > 0) {
+    background = "green-background"
+  }
+
+  else {
+    background = "white-background"
+  }
+
+  // const background = counter === 0 ? "white-background" : counter < 1 ? "red-background" : "green-background";
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${background}`}>
+      <div>
+        <h1>Clicker</h1>
+        <div className='main-panel'>
+          <p>{counter}</p>
+          <div className='buttons'>
+            <button className='red' onClick={downClick}>Down</button>
+            <button className='white' onClick={resetClick}>Reset</button>
+            <button className='green' onClick={upClick}>Up</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
